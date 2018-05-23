b8eb108662ce6a5494f9f0bdd081d7c083142ba2
Your NameWillanzhang <631871612@qq.com>
2018-05-23 18:27:41 +0800

    ok

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import { Button } from 'antd-mobile'
+// import 'antd-mobile/dist/antd-mobile.css'
 import './App.css';
+        // <Button type="primary">1 点击按钮</Button>
 
 class App extends Component {
   render() {
@@ -13,6 +16,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Button type="primary">1 点击按钮</Button>
       </div>
     );
   }
